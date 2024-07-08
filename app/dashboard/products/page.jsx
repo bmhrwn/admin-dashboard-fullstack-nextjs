217b28bd6972812d06e260cf8d760100d67aed40
Bim <bmhrwn@gmail.com>
2024-07-08 08:21:35 +0700

    first commit

---
+++ app/dashboard/products/page.jsx
@@ -0,0 +1,9 @@
+import styles from "../../ui/dashboard/products/products.module.css"
+
+const ProductsPage = () => {
+    return (
+      <div className={styles.container}>ProductsPage</div>
+    )
+  }
+  
+  export default ProductsPage
---
+++ app/dashboard/users/page.jsx
@@ -0,0 +1,18 @@
+import Search from "@/app/ui/dashboard/search/search";
+import styles from "../../ui/dashboard/users/users.module.css";
+import Link from "next/link";
+
+const UsersPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a user..." />
+        <Link href="/dashboard/users/add" />
+        <button className={styles.addButton}>Add New</button>
+      </div>
+      <table className={styles.table}></table>
+    </div>
+  );
+};
+
+export default UsersPage;
